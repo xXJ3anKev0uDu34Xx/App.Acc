4ac1fb772fcd0f72a14c949de7dc186e6a9b788b
nutr4x <nutr4x@gmail.com>
2016-04-29 10:57:09 +0200

    Modification Arborescence

--- AppC/AppC/Form1.Designer.cs
+++ AppC/AppC/Form1.Designer.cs
@@ -28,184 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lbxRef = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.btnSuivant = new System.Windows.Forms.Button();
-            this.btnPrecedent = new System.Windows.Forms.Button();
-            this.tbxArticle = new System.Windows.Forms.TextBox();
-            this.tbxReference = new System.Windows.Forms.TextBox();
-            this.tbxPrix = new System.Windows.Forms.TextBox();
-            this.tbxStock = new System.Windows.Forms.TextBox();
-            this.tbxSeuil = new System.Windows.Forms.TextBox();
-            this.btnAjout = new System.Windows.Forms.Button();
-            this.btnSupprimer = new System.Windows.Forms.Button();
-            this.btnModif = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lbxRef
             // 
             this.lbxRef.FormattingEnabled = true;
-            this.lbxRef.Location = new System.Drawing.Point(12, 12);
+            this.lbxRef.Location = new System.Drawing.Point(12, 62);
             this.lbxRef.Name = "lbxRef";
-            this.lbxRef.Size = new System.Drawing.Size(120, 199);
+            this.lbxRef.Size = new System.Drawing.Size(120, 95);
             this.lbxRef.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(156, 16);
+            this.label1.Location = new System.Drawing.Point(246, 62);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.Size = new System.Drawing.Size(36, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Article ";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(156, 45);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Référence";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(156, 74);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(24, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Prix";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(156, 103);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Stock";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(156, 132);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(70, 13);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Seuil critique ";
-            // 
-            // btnSuivant
-            // 
-            this.btnSuivant.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSuivant.BackgroundImage")));
-            this.btnSuivant.Location = new System.Drawing.Point(211, 165);
-            this.btnSuivant.Name = "btnSuivant";
-            this.btnSuivant.Size = new System.Drawing.Size(47, 46);
-            this.btnSuivant.TabIndex = 6;
-            this.btnSuivant.UseVisualStyleBackColor = true;
-            // 
-            // btnPrecedent
-            // 
-            this.btnPrecedent.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPrecedent.BackgroundImage")));
-            this.btnPrecedent.Location = new System.Drawing.Point(158, 165);
-            this.btnPrecedent.Name = "btnPrecedent";
-            this.btnPrecedent.Size = new System.Drawing.Size(47, 46);
-            this.btnPrecedent.TabIndex = 7;
-            this.btnPrecedent.UseVisualStyleBackColor = true;
-            // 
-            // tbxArticle
-            // 
-            this.tbxArticle.Location = new System.Drawing.Point(229, 13);
-            this.tbxArticle.Name = "tbxArticle";
-            this.tbxArticle.Size = new System.Drawing.Size(329, 20);
-            this.tbxArticle.TabIndex = 8;
-            // 
-            // tbxReference
-            // 
-            this.tbxReference.Location = new System.Drawing.Point(229, 42);
-            this.tbxReference.Name = "tbxReference";
-            this.tbxReference.Size = new System.Drawing.Size(329, 20);
-            this.tbxReference.TabIndex = 9;
-            // 
-            // tbxPrix
-            // 
-            this.tbxPrix.Location = new System.Drawing.Point(229, 71);
-            this.tbxPrix.Name = "tbxPrix";
-            this.tbxPrix.Size = new System.Drawing.Size(329, 20);
-            this.tbxPrix.TabIndex = 10;
-            // 
-            // tbxStock
-            // 
-            this.tbxStock.Location = new System.Drawing.Point(229, 100);
-            this.tbxStock.Name = "tbxStock";
-            this.tbxStock.Size = new System.Drawing.Size(329, 20);
-            this.tbxStock.TabIndex = 11;
-            // 
-            // tbxSeuil
-            // 
-            this.tbxSeuil.Location = new System.Drawing.Point(229, 129);
-            this.tbxSeuil.Name = "tbxSeuil";
-            this.tbxSeuil.Size = new System.Drawing.Size(329, 20);
-            this.tbxSeuil.TabIndex = 12;
-            // 
-            // btnAjout
-            // 
-            this.btnAjout.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAjout.BackgroundImage")));
-            this.btnAjout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.btnAjout.Location = new System.Drawing.Point(407, 165);
-            this.btnAjout.Name = "btnAjout";
-            this.btnAjout.Size = new System.Drawing.Size(47, 46);
-            this.btnAjout.TabIndex = 13;
-            this.btnAjout.UseVisualStyleBackColor = true;
-            // 
-            // btnSupprimer
-            // 
-            this.btnSupprimer.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSupprimer.BackgroundImage")));
-            this.btnSupprimer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.btnSupprimer.Location = new System.Drawing.Point(460, 165);
-            this.btnSupprimer.Name = "btnSupprimer";
-            this.btnSupprimer.Size = new System.Drawing.Size(46, 46);
-            this.btnSupprimer.TabIndex = 14;
-            this.btnSupprimer.UseVisualStyleBackColor = true;
-            // 
-            // btnModif
-            // 
-            this.btnModif.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnModif.BackgroundImage")));
-            this.btnModif.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.btnModif.Location = new System.Drawing.Point(512, 165);
-            this.btnModif.Name = "btnModif";
-            this.btnModif.Size = new System.Drawing.Size(46, 46);
-            this.btnModif.TabIndex = 15;
-            this.btnModif.UseVisualStyleBackColor = true;
+            this.label1.Text = "Article";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(575, 220);
-            this.Controls.Add(this.btnModif);
-            this.Controls.Add(this.btnSupprimer);
-            this.Controls.Add(this.btnAjout);
-            this.Controls.Add(this.tbxSeuil);
-            this.Controls.Add(this.tbxStock);
-            this.Controls.Add(this.tbxPrix);
-            this.Controls.Add(this.tbxReference);
-            this.Controls.Add(this.tbxArticle);
-            this.Controls.Add(this.btnPrecedent);
-            this.Controls.Add(this.btnSuivant);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(446, 385);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lbxRef);
             this.Name = "Form1";
-            this.Text = "Gestion des articles";
+            this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -215,20 +67,6 @@
 
         private System.Windows.Forms.ListBox lbxRef;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button btnSuivant;
-        private System.Windows.Forms.Button btnPrecedent;
-        private System.Windows.Forms.TextBox tbxArticle;
-        private System.Windows.Forms.TextBox tbxReference;
-        private System.Windows.Forms.TextBox tbxPrix;
-        private System.Windows.Forms.TextBox tbxStock;
-        private System.Windows.Forms.TextBox tbxSeuil;
-        private System.Windows.Forms.Button btnAjout;
-        private System.Windows.Forms.Button btnSupprimer;
-        private System.Windows.Forms.Button btnModif;
     }
 }
 
